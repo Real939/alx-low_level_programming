3107c6faba0f8bbb0eee3315136112ce0f1459e9
Real939 <danimulugeta71@gmail.com>
2022-12-10 13:09:54 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
 *Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
-    puts(\"progrmaming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
             return (0);
-
 }
