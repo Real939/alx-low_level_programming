36a20709ee8ac1ff3bca08942647b271c0703f29
Real939 <danimulugeta71@gmail.com>
2022-12-12 06:54:34 -0800

    task 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
-* main - Prints all combination of single digits with comma
+* main - Entry point
 * Return: Always 0
 */
 int main(void)
 {
 int a;
-for (a= 0; a < 100; a++) {
+for (a = 0; a < 100; a++)
+{
 putchar(a + '0');
-if (a < 99) {
+if (a < 99)
+{
 putchar(',');
 putchar(' ');
 }
