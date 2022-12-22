6d5f719df1252aba5b2a475f267fc09b82e96b77
Real939 <danimulugeta71@gmail.com>
2022-12-22 02:59:06 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* _strcat - concatenates two strings
+*main: _strcat - concatenates two strings
 * @dest: input value
 * @src: input value
 * Return: void
 */
+
 char *_strcat(char *dest, char *src)
 {
 int i;
@@ -23,4 +24,5 @@ j++;
 }
 dest[i] = '\n';
 return (dest);
+
 }
